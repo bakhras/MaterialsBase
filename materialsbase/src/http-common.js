f360db1409c869ae913bf1f250ded39a0dec012b
stephenszwiec <Stephen@Szwiec.xyz>
2022-06-30 14:00:29 -0500

    wiping react libs
    restarting react project to pull latest libraries for system

--- materialsbase/src/http-common.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-export default axios.create({
-	baseURL: "https://localhost:8080/api",
-	headers: {
-		"Content-type": "application/json"
-	}
-});
--- materialsbase/src/reducers/index.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from "redux";
-import compounds from "./compounds";
-
-export default combineReducers({
-	compounds
-});
