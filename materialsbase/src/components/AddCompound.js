7722e0a99b38fa1b776e1df2dcdf8f0f96fb15ef
stephenszwiec <Stephen@Szwiec.xyz>
2022-10-13 12:39:02 -0500

    Code cleanup
    - normalized indentation
    - made new functions take same form as the rest of project
    - various cleanup

--- materialsbase/src/components/AddCompound.js
+++ materialsbase/src/components/AddCompound.js
@@ -4,25 +4,27 @@ import { createCompound } from "../actions/compounds";
 import Papa from "papaparse"; /* csv extraction */
 import { Link } from "react-router-dom";
 
-var mol2;
 
 const AddCompound = () => {
+
+	//used to manipulate for file upload purposes
+	var mol2;
+
 	const initialCompoundState = {
 		comp_index: "",
 		comp_material: "",
 		comp_notation: "",
-		comp_mol2: "",
+		comp_mol2: null,
 		comp_components: "",
-		comp_properties: "",
+		comp_properties: null,
 	};
 
 	const [compound, setCompound] = useState(initialCompoundState);
 	const [submitted, setSubmitted] = useState(false);
 
-
-
 	const dispatch = useDispatch();
 	dispatch(createCompound);
+
 	const handleInputChange = event => {
 		const { name, value } = event.target;
 		setCompound({ ...compound, [name]: value });
@@ -45,96 +47,71 @@ const AddCompound = () => {
 		})})
 	};
 
-	//Used to parse Mol2 file into Json Format 
+	//Used to parse Mol2 file into Json Format
 	const handleMolUpload = event => {
 		const { name1, value} = event.target;
-		
-		
-		var file = document.getElementById('comp_mol2').files[0]; 
-		//console.log(document.getElementById('comp_mol2').files[0]); 
-		
+
+		var file = document.getElementById('comp_mol2').files[0];
+		//console.log(document.getElementById('comp_mol2').files[0]);
+
 		const reader = new FileReader();
 
-  reader.addEventListener("load", () => {
-    var mol2String= reader.result;
-	//Splits the Mol file into sectioned array
-	const firstSplit= mol2String.split("@<TRIPOS>");
-	//Splits Each Array into sectioned parts by line
-	var splitList=[];
-	 for (let i=1;i<firstSplit.length;i++){
-		splitList.push(firstSplit[i].split("\n"));
-	 }
-	 console.log(splitList);
-	 var mol2JSON= '{"mol2": {'
-	 for (let i=0;i<splitList.length;i++){
-		if(i==0){
-			mol2JSON+=checkDataRecord(splitList[i]);
-		}
-		else{
-		mol2JSON+=","+checkDataRecord(splitList[i]);
-		if(i==splitList.length-1){
-			mol2JSON+="}}"
-		}
-		}
-	 }
-	 
-	 console.log(mol2JSON);
-	
-  }, false);
+  		reader.addEventListener("load", () => {
+	    		var mol2String = reader.result;
 
-  if (file) {
-    reader.readAsText(document.getElementById('comp_mol2').files[0]);
-  }
-		
-  
-	
+			//Splits the Mol file into sectioned array
+			const firstSplit= mol2String.split("@<TRIPOS>");
+			//Splits Each Array into sectioned parts by line
+			var splitList=[];
+	 		for (let i=1;i<firstSplit.length;i++){
+				splitList.push(firstSplit[i].split("\n"));
+	 		}
+	 		console.log(splitList);
+	 		var mol2JSON= '{"mol2": {'
+	 			for (let i=0;i<splitList.length;i++){
+					if(i==0){
+						mol2JSON+=checkDataRecord(splitList[i]);
+					}
+					else{
+						mol2JSON+=","+checkDataRecord(splitList[i]);
+						if(i==splitList.length-1){
+							mol2JSON+="}}"
+						}
+					}
+	 		}
+	 		console.log(mol2JSON);
 
-	}	
-	function checkDataRecord(dataRecord) {
-		var obj=[];
-		obj=dataRecord;
+  		}, false);
+
+  		if (file) {
+			reader.readAsText(document.getElementById('comp_mol2').files[0]);
+		}
+	};
+
+	//Checks the Data Record and returns the appropriate JSON
+	const checkDataRecord = (dataRecord) => {
 		var recordString="";
-		try {
-			
-		
-		//If Statements to determine type of data record
-		if(obj[0]=="ATOM"){
-			recordString+='"atom":  {"'
-			for (let index = 1; index < obj.length; index++) {
-				if(obj[index]!=""){
-				if (index==1){
-				recordString+=index+'": {"';
+		if(dataRecord[0]=="ATOM"){
+			recordString+= '"atom": [';
+			for (let i=1;i<dataRecord.length;i++){
+				if(dataRecord[i]!=""){
+					recordString+=i+'": {"';
 				}
 				else{
-					recordString+=',"'+index+'": {"';
+					recordString+=',"';+i+'": {"';
 				}
-				var intsplit= obj[index].split(" ").filter( function(e) { return e.trim().length > 0; } );
-					recordString+='atom_id":'+'"'+intsplit[0]+'","';
-					recordString+='atom_name":'+'"'+intsplit[1]+'","';
-					recordString+='x":'+'"'+intsplit[2]+'","';
-					recordString+='y":'+'"'+intsplit[3]+'","';
-					recordString+='z":'+'"'+intsplit[4]+'","';
-					recordString+='atom_type":'+'"'+intsplit[5]+'","';
-					recordString+='subst_id":'+'"'+intsplit[6]+'","';
-					recordString+='subst_name":'+'"'+intsplit[7]+'","';
-					recordString+='charge":'+'"'+intsplit[8]+'"}';
-					
-				}
-				if (index==obj.length-1){
-					recordString+="}";
-				}
-				
 			}
-
 		}
-		else if(obj[0]=="MOLECULE"){
-			recordString+='"molecule":  {"'
-			for (let index = 1; index < 7; index++) {
-				if(index==1){
-					recordString+='mol_name":'+'"'+obj[index]+'","';
+		else if(dataRecord[0]=="MOLECULE"){
+			recordString+= '"molecule": [';
+			for (let i=1;i<dataRecord.length;i++){
+				if(i==1){
+					recordString+='mol_name":'+'"'+dataRecord[i]+'","';
 				}
-				else if(index==2){
-				 var	intsplit=obj[index].split(" ");
+				else if(i==2){
+
+					var intsplit= dataRecord.split(" ");
+
 					recordString+='num_atoms":'+'"'+intsplit[1]+'","';
 					recordString+='num_bonds":'+'"'+intsplit[2]+'","';
 					recordString+='num_subst":'+'"'+intsplit[3]+'","';
@@ -153,40 +130,33 @@ const AddCompound = () => {
 				else if (index==6){
 					recordString+='mol_comment":'+'"'+obj[index]+'"}';
 				}
-				
 			}
-
 		}
-		else if(obj[0]=="BOND"){
-			recordString+='"bond":  {"'
-			for (let index = 1; index < obj.length; index++) {
-				if(obj[index]!=""){
-				if (index==1){
-				recordString+=index+'": {"';
-				}
-				else{
-					recordString+=',"'+index+'": {"';
-				}
-				var intsplit= obj[index].split(" ").filter( function(e) { return e.trim().length > 0; } );
+		else if(dataRecord[0]=="BOND"){
+			recordString+= '"bond": [';
+			for (let i=1;i<dataRecord.length;i++){
+				if(dataRecord[i]!=""){
+					if(i==1){
+						recordString+=i+'": {"';
+					}
+					else{
+						recordString+=',"';+i+'": {"';
+					}
+					var intsplit= dataRecord[i].split(" ").filter( function(f) { return f.trim().length > 0; } );
+
 					recordString+='bond_id":'+'"'+intsplit[0]+'","';
 					recordString+='origin_atom_id":'+'"'+intsplit[1]+'","';
 					recordString+='target_atom_id":'+'"'+intsplit[2]+'","';
 					recordString+='bond_type":'+'"'+intsplit[3]+'"}';
-					
-					
 				}
-				if (index==obj.length-1){
+				if (index==dataRecord.length-1){
 					recordString+="}";
-				}	
+				}
 			}
 		}
-	}
-	catch (error) {
-			alert("An Error Has Occured.");
-	}
-	return recordString;
-		
-	}
+		return recordString;
+	};
+
 	const saveCompound = () => {
 		const {
 			comp_index,
@@ -195,7 +165,7 @@ const AddCompound = () => {
 			comp_mol2,
 			comp_components,
 			comp_properties
-		 } = compound;
+		} = compound;
 
 		dispatch(createCompound(
 			comp_index,
@@ -204,23 +174,22 @@ const AddCompound = () => {
 			comp_mol2,
 			comp_components,
 			comp_properties
-		))
-			.then(data => {
-				setCompound({
-					comp_index: data.comp_index,
-					comp_material: data.comp_material,
-					comp_notation: data.comp_notation,
-					comp_mol2: data.comp_mol2,
-					comp_components: data.comp_components,
-					comp_properties: data.comp_properties,
-        			});
-        			setSubmitted(true);
+		)).then(data => {
+			setCompound({
+				comp_index: data.comp_index,
+				comp_material: data.comp_material,
+				comp_notation: data.comp_notation,
+				comp_mol2: data.comp_mol2,
+				comp_components: data.comp_components,
+				comp_properties: data.comp_properties,
+        		});
 
-        			console.log(data);
-      			})
-			.catch(e => {
-				console.log(e);
-			});
+		setSubmitted(true);
+
+		console.log(data);
+      		}).catch(e => {
+			console.log(e);
+		});
 	};
 
 	const newCompound = () => {
@@ -229,8 +198,7 @@ const AddCompound = () => {
 	};
 
 
-
-  return(
+	return(
 	  <div className="submit-form">
 	  {submitted ? (
 		  <div>
@@ -279,7 +247,7 @@ const AddCompound = () => {
 		  			onChange={handleInputChange}
 		  			name="comp_notation"
 		  		/>
-		  	</div> 
+		  	</div>
 
 			<div className="form-group" >
 				<label htmlFor="comp_mol2">Compound mol2</label>
@@ -292,7 +260,7 @@ const AddCompound = () => {
 		  			onChange={handleMolUpload}
 		  			name="comp_mol2"
 		  		/>
-			
+
 		  	</div>
 			<div className="form-group">
 		  		<label htmlFor="comp_properties">Compound Properties</label>
